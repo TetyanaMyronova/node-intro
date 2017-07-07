d4d6790f4d74e8716ea748c80c117e3c2edb7a56
Tetyana <myronova.tetyana@gmail.com>
2017-07-07 22:35:31 +0000

    Adding getIssPosition, getAddressPosition

--- workshop.js
+++ workshop.js
@@ -1,4 +1,4 @@
-var request = require('request-promise');
+var rp = require('request-promise');
 
 // Euclidian distance between two points
 function getDistance(pos1, pos2) {
@@ -6,18 +6,40 @@ function getDistance(pos1, pos2) {
 }
 
 function getIssPosition() {
-  return request(/* ... */)
+  return rp('http://api.open-notify.org/iss-now.json ')
   .then(
-    function(responce) {
+    function(response) {
       // Parse as JSON
+      var result = JSON.parse(response);
+
+      var coordinates = {
+        "lat": result.iss_position.latitude,
+        "lng": result.iss_position.longitude
+      };
       // Return object with lat and lng
+      return coordinates;
     }
-  )
+  ) 
 }
+
+getIssPosition().then(function(data){console.log(data)});
+
 
 function getAddressPosition(address) {
-
+  return rp('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCacpxCphTH7131rccYn9_3f9quqebrHhM')
+  .then(
+    function(data) {
+      var results = JSON.parse(data);
+      
+      var coordinates = {
+        "lat": results.results[0].geometry.location.lat,
+        "lng": results.results[0].geometry.location.lng
+      };
+      return coordinates;
+    })
 }
+
+//getAddressPosition();
 
 function getCurrentTemperatureAtPosition(position) {
 
