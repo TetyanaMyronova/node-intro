8a74348a2e7b57cfb54526823893ec08b8345c32
Tetyana <myronova.tetyana@gmail.com>
2017-07-10 10:44:10 +0000

    Small changes, added getDistanceFromIss

--- workshop.js
+++ workshop.js
@@ -1,9 +1,10 @@
 var rp = require('request-promise');
 
-var address = '1600+Amphitheatre+Parkway,+Mountain+View,';
+var address = '130+Rue+de+la+Barre,+Longueuil,+CA';
 
 // Euclidian distance between two points
 function getDistance(pos1, pos2) {
+
   return Math.sqrt(Math.pow(pos1.lat - pos2.lat, 2) + Math.pow(pos1.lng - pos2.lng, 2));
 }
 
@@ -19,6 +20,7 @@ function getIssPosition() {
         "lng": result.iss_position.longitude
       };
       // Return object with lat and lng
+      //console.log(coordinates);
       return coordinates;
     }
   ) 
@@ -28,7 +30,8 @@ function getIssPosition() {
 
 
 function getAddressPosition(address) {
-  return rp('https://maps.googleapis.com/maps/api/geocode/json?address='+address+'+CA&key=AIzaSyCacpxCphTH7131rccYn9_3f9quqebrHhM')
+  var api_key = 'AIzaSyCacpxCphTH7131rccYn9_3f9quqebrHhM'
+  return rp('https://maps.googleapis.com/maps/api/geocode/json?address='+address+'&key='+api_key)
   .then(
     function(data) {
       var results = JSON.parse(data);
@@ -51,7 +54,7 @@ function getCurrentTemperatureAtPosition(coordinates) {
         var result = JSON.parse(data);
         
         var temperature = result.currently.temperature;
-        
+        //console.log(temperature);
         return temperature;
     })
 }
@@ -59,24 +62,23 @@ function getCurrentTemperatureAtPosition(coordinates) {
 //getCurrentTemperatureAtPosition(coordinates);
 
 
-
-
 function getCurrentTemperature(address) {
   getAddressPosition(address)
    .then(
      function(coordinates){
-        getCurrentTemperatureAtPosition(coordinates)
-        .then(
-          function(data){
-            var result = data;
-              console.log(data);
-              return data;
-          })
+       return getCurrentTemperatureAtPosition(coordinates)
      })
 }
 
-getCurrentTemperature();
+//getCurrentTemperature();
 
 function getDistanceFromIss(address) {
-
+  Promise.all([getAddressPosition(address), getIssPosition()])
+  .then(function(data){
+    console.log(data);
+    console.log("The distance between me and orbital satellite is "+ getDistance(data[0], data[1]) + " kilometers.");
+    return getDistance(data[0], data[1]);
+  })
 }
+
+getDistanceFromIss();
